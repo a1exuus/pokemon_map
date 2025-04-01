86e8dd4950b375218ddc318a4f415c8bd254f4f5
a1exuus <162327737+a1exuus@users.noreply.github.com>
2025-04-01 18:15:14 +0300

    added `null=True` and `blank=True` in the right places

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,7 @@ from django.utils.timezone import now
 
 
 class Pokemon(models.Model):
-    title_ru = models.CharField(max_length=200, blank=True, verbose_name='Название(рус.)')
+    title_ru = models.CharField(max_length=200, verbose_name='Название(рус.)')
     title_en = models.CharField(max_length=200, blank=True, verbose_name='Название(анг.)')
     title_jp = models.CharField(max_length=200, blank=True, verbose_name='Название(яп.)')
     image = models.ImageField(null=True, verbose_name='Изображение')
@@ -26,10 +26,10 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
-    appeared_at = models.DateTimeField(default=now, verbose_name='Когда появился')
+    appeared_at = models.DateTimeField(default=now, verbose_name='Когда появился(тся)')
     disappeared_at = models.DateTimeField(default=now, verbose_name='Когда пропадет(ал)')
-    level = models.IntegerField(default=3, verbose_name='Уровень')
-    health = models.IntegerField(default=3, verbose_name='Здоровье')
-    strength = models.IntegerField(default=3, verbose_name='Сила')
-    defence = models.IntegerField(default=3, verbose_name='Защита')
-    stamina = models.IntegerField(default=3, verbose_name='Выносливость')
+    level = models.IntegerField(null=True, verbose_name='Уровень')
+    health = models.IntegerField(null=True, verbose_name='Здоровье')
+    strength = models.IntegerField(null=True, verbose_name='Сила')
+    defence = models.IntegerField(null=True, verbose_name='Защита')
+    stamina = models.IntegerField(null=True, verbose_name='Выносливость')
