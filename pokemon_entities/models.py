1ddb2e8f0b2af933f6ae7a320b08897237b8eb8f
a1exuus <162327737+a1exuus@users.noreply.github.com>
2025-04-01 18:08:46 +0300

    added `verbose_name` to the every field in models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,17 +3,18 @@ from django.utils.timezone import now
 
 
 class Pokemon(models.Model):
-    title_ru = models.CharField(max_length=200, blank=True)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(null=True)
-    description = models.CharField(max_length=1000, blank=True)
+    title_ru = models.CharField(max_length=200, blank=True, verbose_name='Название(рус.)')
+    title_en = models.CharField(max_length=200, blank=True, verbose_name='Название(анг.)')
+    title_jp = models.CharField(max_length=200, blank=True, verbose_name='Название(яп.)')
+    image = models.ImageField(null=True, verbose_name='Изображение')
+    description = models.CharField(max_length=1000, blank=True, verbose_name='Описание')
 
     evolved_from = models.ForeignKey(
         'self', 
         on_delete=models.CASCADE, 
         null=True, blank=True, 
-        related_name='next_evolution'
+        related_name='next_evolution',
+        verbose_name='Из кого эволюционировал'
     )
 
     def __str__(self):
@@ -22,13 +23,13 @@ class Pokemon(models.Model):
     
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField(default=now)
-    disappeared_at = models.DateTimeField(default=now)
-    level = models.IntegerField(default=3)
-    health = models.IntegerField(default=3)
-    strength = models.IntegerField(default=3)
-    defence = models.IntegerField(default=3)
-    stamina = models.IntegerField(default=3)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
+    lat = models.FloatField(verbose_name='Широта')
+    lon = models.FloatField(verbose_name='Долгота')
+    appeared_at = models.DateTimeField(default=now, verbose_name='Когда появился')
+    disappeared_at = models.DateTimeField(default=now, verbose_name='Когда пропадет(ал)')
+    level = models.IntegerField(default=3, verbose_name='Уровень')
+    health = models.IntegerField(default=3, verbose_name='Здоровье')
+    strength = models.IntegerField(default=3, verbose_name='Сила')
+    defence = models.IntegerField(default=3, verbose_name='Защита')
+    stamina = models.IntegerField(default=3, verbose_name='Выносливость')
